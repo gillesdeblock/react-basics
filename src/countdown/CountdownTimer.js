2a359536a353da1ee4d03b86a30aee058a1c19a6
gillesdeblock <gillesdeblock2@gmail.com>
2025-10-28 17:26:22 +0100

    refactor: wrote custom useCountdown hook + DRY

--- src/countdown/CountdownTimer.js
+++ src/countdown/CountdownTimer.js
@@ -1,20 +1,42 @@
 import { useEffect, useRef, useState } from "react";
 
 function CountdownTimer() {
-    const initialSeconds = useRef(60);
+    const {
+        seconds,
+        isCountingDown,
+        setInitialSeconds,
+        start,
+        stop,
+    } = useCountdown(60);
+
+    return (
+        <div className="flex flex-column self-start">
+            <div className="flex gap-2">
+                <CountdownTimerInput seconds={seconds} disabled={isCountingDown} onInput={setInitialSeconds}></CountdownTimerInput>
+                {!isCountingDown && <CountdownTimerButton disabled={seconds <= 0} onClick={start}>Start</CountdownTimerButton>}
+                {isCountingDown && <CountdownTimerButton onClick={stop}>Stop</CountdownTimerButton>}
+            </div>
+        </div>
+    )
+}
+
+function useCountdown(initial = 60) {
+    const initialSeconds = useRef(initial);
     const intervalID = useRef(null);
 
     const [seconds, setSeconds] = useState(initialSeconds.current);
     const [isCountingDown, setIsCountingDown] = useState(false);
+    const isFinished = seconds === 0;
 
     const start = () => setIsCountingDown(true);
-    const stop = () => setIsCountingDown(false);
-    const reset = () => setSeconds(initialSeconds.current);
-
-    const onInput = (e) => {
+    const stop = () => {
+        setIsCountingDown(false);
+        setSeconds(initialSeconds.current);
+    };
+    const setInitialSeconds = (e) => {
         const { value } = e.target;
-        setSeconds(value);
         initialSeconds.current = value;
+        setSeconds(value);
     };
 
     useEffect(() => {
@@ -26,31 +48,32 @@ function CountdownTimer() {
 
     useEffect(() => {
         clearInterval(intervalID.current);
-    }, [seconds === 0]);
+    }, [isFinished]);
 
+    return { seconds, isCountingDown, start, stop, setInitialSeconds };
+}
+
+function CountdownTimerInput({ seconds, disabled, onInput }) {
     return (
-        <div className="flex flex-column self-start">
-            <div className="flex gap-2">
-                <input
-                    type="number"
-                    className="outline-none px-2 py-1 border bg-transparent"
-                    value={seconds}
-                    onInput={onInput}
-                    min={1}
-                    disabled={isCountingDown}
-                />
-                {
-                    !isCountingDown && <div className="flex gap-2">
-                        <button className="border px-2 py-1 hover:bg-gray-50 disabled:bg-gray-100" disabled={!(seconds > 0)} onClick={start}>Start</button>
-                        {
-                            (initialSeconds.current !== seconds) && <button className="border px-2 py-1 hover:bg-gray-50" onClick={reset}>Reset ({initialSeconds.current})</button>
-                        }
-                    </div>
-                }
-                {isCountingDown && <button className="border px-2 py-1 hover:bg-gray-50" onClick={stop}>Stop</button>}
-            </div>
-        </div>
-    )
+        <input
+            type="number"
+            className="outline-none px-2 py-1 border bg-transparent"
+            value={seconds}
+            onInput={onInput}
+            min={1}
+            disabled={disabled}
+        />
+    );
+}
+
+function CountdownTimerButton({ disabled, onClick, children }) {
+    return <button
+        className="border px-2 py-1 hover:bg-gray-50 active:focus:bg-gray-100 disabled:bg-gray-100"
+        disabled={disabled}
+        onClick={onClick}
+    >
+        {children}
+    </button>
 }
 
 export default CountdownTimer;
